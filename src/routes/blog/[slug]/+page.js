826fd4f57094eb7ffe37dfba07ffcf7ae558a84d
Shinichi Okada <okada.shin@gmail.com>
2022-10-27 06:45:51 +0900

    change from npm to pnpm

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,11 +1,17 @@
-export async function load({ params }) {
-	const post = await import(`../${params.slug}.md`);
-	const { title, dir } = post.metadata;
-	const content = post.default;
+// src/routes/blog/[slug]/+page.js
+import { error } from '@sveltejs/kit';
 
-	return {
-		content,
-		title,
-		dir
-	};
+/** @type {import('./$types').PageLoad} */
+export async function load ({ params }) {
+  const post = await import(`../${params.slug}.md`);
+  const { title, dir } = post.metadata;
+  const content = post.default;
+
+  return {
+    content,
+    title,
+    dir
+  };
+
+  throw error(404, 'Not found');
 }
