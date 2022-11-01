27898b5ac5a3e10e6dd9e9330fc037e03f6cccc5
Shinichi Okada <okada.shin@gmail.com>
2022-11-02 06:46:36 +0900

    feat: using svelte files to render pages

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -8,10 +8,12 @@ export const GET = async () => {
 const fetchMarkdownPosts = async () => {
 	const articleFiles = import.meta.glob('/src/routes/blog/*.md');
 	const pageFiles = import.meta.glob('/src/routes/pages/*.md');
+	const svelteFiles = import.meta.glob('/src/routes/svelte/*.svelte');
 
 	// returns an array of files
 	const iterableArticleFiles = Object.entries(articleFiles);
 	const iterablePageFiles = Object.entries(pageFiles);
+	const iterableSvelteFiles = Object.entries(svelteFiles);
 
 	const articles = await Promise.all(
 		iterableArticleFiles.map(async ([path, resolver]) => {
@@ -35,5 +37,16 @@ const fetchMarkdownPosts = async () => {
 		})
 	);
 
-	return { pages, articles };
+	const sveltefiles = await Promise.all(
+		iterableSvelteFiles.map(async ([path, resolver]) => {
+			const { metadata } = await resolver();
+			const filePath = path.slice(18, -7);
+			return {
+				meta: metadata,
+				path: filePath
+			};
+		})
+	);
+
+	return { pages, articles, sveltefiles };
 };
